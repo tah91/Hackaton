b043b3f0e7f8c463dde2f9846c6a45382ecea8c6
Tahir <tah.iftikhar@gmail.com>
2011-09-17 13:55:43 +0200

    initial commit

---
+++ Hackaton/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackaton.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Buisness man, welcome";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
+}
